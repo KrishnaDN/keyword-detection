4374f00669890b12ea92b21bfa2ac5288a74ad18
Krishna D N <krishnadn94@gmail.com>
2021-05-19 13:58:57 +0530

    featurization

---
+++ egs/speech_commands/train.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+
+export CUDA_VISIBLE_DEVICES="0"
+stage=0 # start from 0 if you need to start from data preparation
+stop_stage=6
+# data
+nj=40
+feat_dir=fbank
+dict=data/dict/lang_char.txt
+manifest=manifest
+train_set=train_sp
+data_url=https://storage.googleapis.com/download.tensorflow.org/data/speech_commands_v0.02.tar.gz
+
+cmvn=true
+compress=true
+fbank_conf=conf/fbank.conf
+dir=exp/fbank_sp
+
+. utils/parse_options.sh || exit 1;
+
+mkdir -p $manifest
+
+### Download dataset
+#wget $data_url
+#mkdir data2
+#mv ./speech_commands_v0.02.tar.gz ./data2
+#cd ./data2
+#tar -xf ./speech_commands_v0.02.tar.gz
+#cd ../
+
+### data preperation
+#local/dataset_v2.py --dataset_path ./data2
+utils/perturb_data_dir_speed.sh 0.9 data/train data/train_sp0.9
+utils/perturb_data_dir_speed.sh 1.1 data/train data/train_sp1.1
+utils/combine_data.sh data/train_sp data/train data/train_sp0.9 data/train_sp1.1
+
+if [ ${stage} -le 1 ]; then
+    # Feature extraction
+    mkdir -p $feat_dir
+    for x in ${train_set} test valid; do
+        cp -r data/$x $feat_dir
+        steps/make_fbank.sh --cmd "$train_cmd" --nj $nj \
+            --write_utt2num_frames true --fbank_config $fbank_conf --compress $compress $feat_dir/$x
+    done
+    if $cmvn; then
+        compute-cmvn-stats --binary=false scp:$feat_dir/$train_set/feats.scp \
+            $feat_dir/$train_set/global_cmvn
+    fi
+fi
+exit 1
+python format_data.py --feat_scp fbank/train_sp/feats.scp --text_file data/train_sp/text --cmvn_file fbank/train_sp/global_cmvn --store_folder /media/newhd/Google_Speech_Commands/features/train --manifest manifest/train
+python format_data.py --feat_scp fbank/test/feats.scp --text_file data/test/text --cmvn_file fbank/train_sp/global_cmvn --store_folder /media/newhd/Google_Speech_Commands/features/test --manifest manifest/test
+python format_data.py --feat_scp fbank/valid/feats.scp --text_file data/valid/text --cmvn_file fbank/train_sp/global_cmvn --store_folder /media/newhd/Google_Speech_Commands/features/valid --manifest manifest/valid
